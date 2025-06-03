8195b6fdbc012ca2781614082f9fa1bae38284bd
Mike Mayo <mikemayo482@gmail.com>
2025-06-03 11:38:21 +0800

    6th Commit (Login Page Inprogress)

--- DevLab/src/App.jsx
+++ DevLab/src/App.jsx
@@ -3,7 +3,7 @@ import Login from "./components/Login"
 function App() {
   return (
     <>
-      <div className="text-xs">Hello World</div>
+      <Login/>
     </>
   )
 }
--- DevLab/src/components/Login.jsx
+++ DevLab/src/components/Login.jsx
@@ -1,8 +1,24 @@
 
+import Image from '../assets/Images/Login-bg.png';
+
 function Login() {
     
     return (
-        <div className="text-6xl">Login</div>
+        <div className="min-h-screen bg-[#0D1117] flex justify-center items-center">
+
+            {/* Login Wrapper*/}
+            <div className="h-[70vh] w-[70%] bg-[#25293B] rounded-4xl shadow-lg shadow-cyan-500">
+
+            {/*Left pannel*/}
+            <div className="bg-[url('../assets/Images/Login-bg.png')] bg-cover h-[100%] w-[50%] rounded-4xl flex justify-center ">
+            <h1 className="self-center font-exo text-[3.5rem] text-[#F5F5F5]">DEVLAB</h1>
+            </div>
+
+            </div>
+
+
+
+        </div>
     )
 }
 export default Login
